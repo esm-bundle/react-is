21191fe10328f9c49a9455a93a6803b8d348f243
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-14 15:54:29 -0600

    Build react-is

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,28 @@
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 
 function createConfig(format) {
   const dir = format === "module" ? "esm" : format;
   return {
-    input: require.resolve("autopublish-template"),
+    input: `src/index.js`,
     output: {
       file: `${dir}/index.js`,
       sourcemap: true,
       format,
+      exports: "named",
     },
-    plugins: [commonjs(), terser()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      replace({
+        values: {
+          "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+      }),
+      terser(),
+    ],
   };
 }
 
