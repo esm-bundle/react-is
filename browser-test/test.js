edcfc7a58ada9cdcdadc9f1c9d1cf786fbff28b3
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-14 16:01:49 -0600

    Update tests

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,19 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/react-is", () => {
   it("can load the ESM bundle", () => {
     return import("/base/esm/index.js");
   });
 
   it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+    return System.import("/base/system/index.js").then((module) => {
+      expect(module.default).toBeDefined();
+      expect(module.__esModule).toBeDefined();
+      // Named export
+      expect(typeof module.isValidElementType).toBe("function");
+      // Default export
+      expect(typeof module.default.isValidElementType).toBe("function");
+      // Non function export
+      expect(module.default.Element).toBeDefined();
+      expect(module.Element).toBeDefined();
+    });
   });
 });
--- node-test/main.test.mjs
+++ node-test/main.test.mjs
@@ -1,5 +1,14 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/react-is", () => {
   it("can load the esm bundle without dying", () => {
     return import("../esm/index.js");
+  });
+  it(`API exports are defined`, async () => {
+    const { default: ReactIs, isValidElementType, Element } = await import(
+      "../esm/index.js"
+    );
+    expect(ReactIs.isValidElementType).not.to.equal(undefined);
+    expect(isValidElementType).not.to.equal(undefined);
+    expect(ReactIs.Element).not.to.equal(undefined);
+    expect(Element).not.to.equal(undefined);
   });
 });
