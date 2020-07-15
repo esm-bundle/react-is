65355a77f07ecb1ef6292f61e32b8eb8b4665343
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-14 20:52:35 -0600

    Add tests for development bundle

--- browser-test/test.js
+++ browser-test/test.js
@@ -3,8 +3,28 @@ describe("@esm-bundle/react-is", () => {
     return import("/base/esm/react-is.production.min.js");
   });
 
+  it("can load the development ESM bundle", () => {
+    return import("/base/esm/react-is.development.js");
+  });
+
   it("can load the production System.register bundle", () => {
     return System.import("/base/system/react-is.production.min.js").then(
+      (module) => {
+        expect(module.default).toBeDefined();
+        expect(module.__esModule).toBeDefined();
+        // Named export
+        expect(typeof module.isValidElementType).toBe("function");
+        // Default export
+        expect(typeof module.default.isValidElementType).toBe("function");
+        // Non function export
+        expect(module.default.Element).toBeDefined();
+        expect(module.Element).toBeDefined();
+      }
+    );
+  });
+
+  it("can load the development System.register bundle", () => {
+    return System.import("/base/system/react-is.development.js").then(
       (module) => {
         expect(module.default).toBeDefined();
         expect(module.__esModule).toBeDefined();
--- node-test/main.test.mjs
+++ node-test/main.test.mjs
@@ -2,9 +2,24 @@ describe("@esm-bundle/react-is", () => {
   it("can load the production esm bundle without dying", () => {
     return import("../esm/react-is.production.min.js");
   });
+
+  it("can load the development esm bundle without dying", () => {
+    return import("../esm/react-is.development.js");
+  });
+
   it(`API exports are defined in production bundle`, async () => {
     const { default: ReactIs, isValidElementType, Element } = await import(
       "../esm/react-is.production.min.js"
+    );
+    expect(ReactIs.isValidElementType).not.to.equal(undefined);
+    expect(isValidElementType).not.to.equal(undefined);
+    expect(ReactIs.Element).not.to.equal(undefined);
+    expect(Element).not.to.equal(undefined);
+  });
+
+  it(`API exports are defined in development bundle`, async () => {
+    const { default: ReactIs, isValidElementType, Element } = await import(
+      "../esm/react-is.development.js"
     );
     expect(ReactIs.isValidElementType).not.to.equal(undefined);
     expect(isValidElementType).not.to.equal(undefined);
